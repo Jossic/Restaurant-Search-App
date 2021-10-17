89c0cbca1730eee1243aeecf33f9f9c560e68604
Jossic <jossic.lapierre@gmail.com>
2021-10-17 15:49:12 +0200

    Fin 94

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 const SearchBar = () => {
 	return (
 		<View style={styles.background}>
-			<Text>Search Bar</Text>
+			<Text>
+				<Feather name='search' size={30} color='black' />
+				Search Bar
+			</Text>
 		</View>
 	);
 };
