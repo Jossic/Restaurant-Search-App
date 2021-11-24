301fd521dfbcf1fe1140ba04e97291c19c9f5573
Jossic <jossic.lapierre@gmail.com>
2021-11-24 13:43:18 +0100

    Fin 110

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 const ResultsDetail = ({ result }) => {
 	return (
 		<View>
-			<Text>{result.name}</Text>
+			<Image style={styles.image} source={{ uri: result.image_url }} />
+			<Text style={styles.name}>{result.name}</Text>
+			<Text style={styles.name}>
+				{result.rating} Stars, {result.review_count} Reviews
+			</Text>
 		</View>
 	);
 };
 
 export default ResultsDetail;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	image: {
+		height: 120,
+		width: 250,
+		borderRadius: 4,
+	},
+	name: {
+		fontWeight: 'bold',
+		fontSize: 16,
+	},
+});
