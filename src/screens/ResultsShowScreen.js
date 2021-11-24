568f8041f6eef57c4fe605f81f8c10879fd45bcc
Jossic <jossic.lapierre@gmail.com>
2021-11-24 14:35:09 +0100

    Fin 120

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import yelp from '../api/yelp';
 
 const ResultsShowScreen = ({ navigation }) => {
+	const [result, setResult] = useState(null);
 	const id = navigation.getParam('id');
+
+	const getResult = async (id) => {
+		const response = await yelp.get(`/${id}`);
+		setResult(response.data);
+	};
+
+	useEffect(() => {
+		getResult(id);
+	}, []);
+
 	return (
 		<View>
 			<Text> Results Show </Text>
